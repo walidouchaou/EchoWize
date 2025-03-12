50e4d6d2d3977673f72ae669aec64a5591ad4ed8
Walid OUCHAOU <45299069+walidouchaou@users.noreply.github.com>
2025-03-13 00:03:52 +0100

    Update data_transformer.py
    update configue s3

--- api/services/data_transformer.py
+++ api/services/data_transformer.py
@@ -1,4 +1,4 @@
-from typing import Dict, Any
+
 import duckdb
 from dotenv import load_dotenv
 import os
@@ -10,17 +10,18 @@ class DataTransformer:
         self._configure_s3()
     
     def _configure_s3(self):
-        self.con.query("""
+        self.con.query(f"""
             INSTALL httpfs;
             LOAD httpfs;
             SET s3_region='eu-east-1';
             SET s3_url_style='path';
-            SET s3_endpoint='localhost:9000';
-            SET s3_access_key_id='1hDxjeNAlqUuVQqHQZbO';
-            SET s3_secret_access_key='M2FsRid6WCDQCZNh7x0gmNj7IitM9qKlxlqFhEY0';
+            SET s3_endpoint='{os.getenv("MINIO_ENDPOINT")}';
+            SET s3_access_key_id='{os.getenv("MINIO_ACCESS_KEY")}';
+            SET s3_secret_access_key='{os.getenv("MINIO_SECRET_KEY")}';
             SET s3_use_ssl=false;
         """)
-    
+
+
     def transform_reviews(self):
         self.con.query("""CREATE OR REPLACE TABLE conso_reviews AS 
             SELECT *
